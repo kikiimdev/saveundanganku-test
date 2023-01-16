f820b6658ae785d96cd74daa4ee647c70ca745c4
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2023-01-16 21:29:56 +0800

    build: update url

---
+++ _nuxt/composables.965a52ee.js
@@ -0,0 +1,1 @@
+import{a}from"./entry.c5ce85ab.js";function s(e,u){return a()._useHead(e,u)}export{s as u};
--- _nuxt/composables.da9fedcf.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./entry.4090701b.js";function s(e,u){return a()._useHead(e,u)}export{s as u};
