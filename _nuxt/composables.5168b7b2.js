dc2d2fec627b282f8dd5a7eb7d988941c35623fb
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2023-01-16 21:35:31 +0800

    feat: add index page

---
+++ _nuxt/composables.5168b7b2.js
@@ -0,0 +1,1 @@
+import{a}from"./entry.168bb45b.js";function s(e,u){return a()._useHead(e,u)}export{s as u};
--- _nuxt/composables.965a52ee.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./entry.c5ce85ab.js";function s(e,u){return a()._useHead(e,u)}export{s as u};
